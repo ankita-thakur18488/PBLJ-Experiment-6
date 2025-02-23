7e635aeeb46066d7bf337a98674e4c932ef82c57
ankita-thakur18488 <ankita.e18488@cumail.in>
2025-02-23 15:17:15 +0530

    Update Exp6.3.java

--- Exp6.3.java
+++ Exp6.3.java
@@ -1,1 +1,36 @@
+To develop a Java program that processes a large dataset of products using Streams class to:
+  - Group products by category
+  - Find the most expensive product in each category
+  - Calculate the average price of all products
 
+
+Instruction
+Step 1: Create the Product Class
+- Define a Product class with attributes:
+    name (String)
+    category (String)
+    price (double)
+
+For Example: "Laptop", "Electronics", 1200
+             "Phone", "Electronics", 800
+  
+- Implement a constructor to initialize these values.
+- Override toString() to print product details.
+
+Step 2: Create the ProductProcessor Class
+- Create a list of products with multiple categories and prices.
+- Use Streams API to:
+    Group products by category using Collectors.groupingBy().
+    Find the most expensive product in each category using Collectors.maxBy().
+    Calculate the average price of all products using Collectors.averagingDouble().
+- Display the results.
+
+
+  
+Test Cases
+Test Case	                        Input Data	                                                                           Expected Output
+Case 1: Normal Case             	Sample dataset with Electronics, Clothing, and Footwear	                      Grouped products, Most Expensive per category, Average price
+Case 2: Single Category Only      All products in "Electronics"	                                                One category, Most Expensive in Electronics, Average of all
+Case 3: Same Price in a Category	Two products with the same highest price in "Footwear"	                      Any of the most expensive ones is displayed
+Case 4: Only One Product	        One product "Laptop" in "Electronics"	                                        Laptop as the most expensive, Laptop as the only average
+Case 5: Empty List	              No products	                                                                  No grouping, No most expensive product, Average price = 0
